762fe224078111c393ccbf2d569e929d2fb6d16b
drake7 <deepak.kumar7v@gmail.com>
2022-10-01 21:56:42 -0700

    Adding Booking repository

--- org/org/src/main/java/careIOv2/org/controller/BookingJpaController.java
+++ org/org/src/main/java/careIOv2/org/controller/BookingJpaController.java
@@ -1,5 +1,10 @@
 package careIOv2.org.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BookingJpaController {
 
+	
+	
 }
---
+++ org/org/src/main/java/careIOv2/org/dao/BookingJpaRepository.java
@@ -0,0 +1,31 @@
+package careIOv2.org.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Repository;
+
+import careIOv2.org.entity.Booking;
+import careIOv2.org.entity.User;
+
+@Repository
+@Transactional
+public class BookingJpaRepository {
+
+	//@Autowired can work too but better if we use
+		@PersistenceContext
+		private EntityManager entityManager;
+		
+		public void insert(Booking booking)
+		{
+			entityManager.merge(booking);
+		}
+		
+		
+		
+		public Booking findByBookingId(long bookingId)
+		{
+			return entityManager.find(Booking.class, bookingId);
+		}
+}
