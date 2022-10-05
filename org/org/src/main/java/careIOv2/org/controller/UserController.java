46b51caf32f15c9e94bd409a76d8fb17313f659d
maccimac <macci.web@gmail.com>
2022-10-05 15:00:48 -0700

    fix cors issue; fix booking_id typo

--- org/org/src/main/java/careIOv2/org/controller/UserController.java
+++ org/org/src/main/java/careIOv2/org/controller/UserController.java
@@ -1,6 +1,7 @@
 package careIOv2.org.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,6 +30,8 @@ public class UserController {
 		this.service = service;
 	}
 
+
+	@CrossOrigin
 	@GetMapping("/users")
 	public List<User> retrieveAllUsers() {
 
